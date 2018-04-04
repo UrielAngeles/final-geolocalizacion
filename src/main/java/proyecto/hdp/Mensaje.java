88d5b737b064514f7c437336d6094f6837257e83
UriDrack7 <urielangeles96@gmail.com>
2018-04-03 21:42:47 -0500

    1ra Parte del proyecto Creacion de clases y encapsulamiento y constructores

---
+++ src/main/java/proyecto/hdp/Mensaje.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.hdp;
+
+/**
+ *
+ * @author UriDrack7
+ */
+public class Mensaje {
+    private String contenido;
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public Mensaje() {
+    }
+}
---
+++ src/main/java/proyecto/hdp/Usuario.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.hdp;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author UriDrack7
+ */
+public class Usuario {
+    private String id;
+    private String nickname;
+    private String mail;
+    private String password;
+    private ArrayList<Mensaje> mensajes;
+    private Posicion posicion;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<Mensaje> getMensajes() {
+        return mensajes;
+    }
+
+    public void setMensajes(ArrayList<Mensaje> mensajes) {
+        this.mensajes = mensajes;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+}
