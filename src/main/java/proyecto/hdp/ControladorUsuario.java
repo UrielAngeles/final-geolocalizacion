fade970c48797889d53c43c51f1493048c48bac2
UriDrack7 <urielangeles96@gmail.com>
2018-04-23 10:23:40 -0500

    Ultima parte del Proyecto!!!

--- src/main/java/proyecto/hdp/ControladorUsuario.java
+++ src/main/java/proyecto/hdp/ControladorUsuario.java
@@ -6,8 +6,10 @@
 package proyecto.hdp;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,5 +39,45 @@ public class ControladorUsuario {
         repo.save(u);
         e.setSuccess(true);
         return e;
+    }
+    
+        //Borrar
+    @CrossOrigin
+    @RequestMapping(value="/usuario/{Id}", method= RequestMethod.DELETE, 
+            headers={"Accept=application/json"})
+    public Estatus borrarMensaje(@PathVariable String Id){
+    
+        Estatus estatus=new Estatus(true, "Borrado con Exito");
+        repo.delete(new Usuario(Id));
+        
+        return estatus;
+    }
+    
+    //Buscar Todos
+    @CrossOrigin
+    @RequestMapping(value = "/mensajito",method = RequestMethod.GET,
+            headers = {"Accept=application/json"})
+    public ArrayList<Usuario> hola(){
+      
+        return(ArrayList<Usuario>) repo.findAll(); 
+    }
+    
+    //Buscar por ID
+    @CrossOrigin
+    @RequestMapping(value = "/mensajito/{Id}",method = RequestMethod.GET,
+            headers = {"Accept=application/json"})
+    public Usuario hola(@PathVariable String Id){
+      
+        return repo.findOne(Id); 
+    }
+    
+    //Actualizar
+    @CrossOrigin
+    @RequestMapping(value="/usuario/{Id}/{titulo}/{cuerpo}",method=RequestMethod.PUT,
+            headers={"Accept=application/json"})
+    
+    public Estatus actualizar(@PathVariable String Id,@PathVariable String titulo, @PathVariable String cuerpo){
+        repo.save(new Usuario(Id, titulo,cuerpo));
+        return new Estatus(true, "Actualizado con exito");
     }
 }
